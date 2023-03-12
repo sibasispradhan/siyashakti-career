14c35db4cad7ea230b99713044d9858b86439f21
Sibasis Pradhan <125789985+sibasispradhan@users.noreply.github.com>
2023-03-12 10:41:52 +0000

    connect with mysqldb

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db, load_jobs_list_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
-
-JOBS = [{
-  "id": 1,
-  "title": "Data Analyst",
-  "location": "Bengaluru, India",
-  "salary": "Rs. 1,00,000"
-}, {
-  "id": 2,
-  "title": "Data Scientist",
-  "location": "Delhi, India",
-  "salary": "Rs. 1,50,000"
-}, {
-  "id": 1,
-  "title": "Frontend Engineer",
-  "location": "Remote"
-}, {
-  "id": 1,
-  "title": "Backend Enginner",
-  "location": "San Francisco, USA",
-  "salary": "$ 120,000"
-}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Company')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Company')
 
 
 @app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_list_db()
+  return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == '__main__':
