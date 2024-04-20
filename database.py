8782140783e8b6b0fd6c1ffa43c4ab3485c3e9da
Sibasis Pradhan <125789985+sibasispradhan@users.noreply.github.com>
2024-04-20 09:52:40 +0530

    Update database.py
    connection changes

--- database.py
+++ database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine, text
 import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
+engine = create_engine(db_connection_string)
 
+"""
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
-
+"""
 
 def load_jobs_from_db():
   with engine.connect() as conn:
